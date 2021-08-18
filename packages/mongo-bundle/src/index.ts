3bc4312a938ce6722ec4365015d256c254de2a91
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-08-18 12:59:08 +0300

    (fix) updates

--- packages/mongo-bundle/src/index.ts
+++ packages/mongo-bundle/src/index.ts
@@ -3,7 +3,6 @@ export { Behaviors } from "./behaviors";
 export * from "./services/DatabaseService";
 export * from "./services/MigrationService";
 export { MongoBundle } from "./MongoBundle";
-export { Exclude, Expose, Type, Transform } from "class-transformer";
 export { ObjectId, ObjectId as ObjectID } from "@bluelibs/ejson";
 export * from "./defs";
 export * from "./events";
