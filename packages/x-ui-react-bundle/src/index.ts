673b6e7a83d022a95f532bf3aa9aa4fb35eb0de1
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-10 16:43:11 +0200

    fix(x-ui): exporting and merger of x-ui-admin interface

--- packages/x-ui-react-bundle/src/index.ts
+++ packages/x-ui-react-bundle/src/index.ts
@@ -1,6 +1,4 @@
 export * from "./defs";
 export * from "./constants";
-
 export * from "./react";
-
 export * from "./XUIReactBundle";
--- packages/x-ui/src/index.ts
+++ packages/x-ui/src/index.ts
@@ -8,3 +8,18 @@ export * from "@bluelibs/ui-apollo-bundle";
 
 export * from "./XUIBundle";
 export * from "./defs";
+
+/**
+ * The code below is designed to offer backwards compatibility
+ * We recommend that interface extension should be done on `@bluelibs/x-ui-react-bundle` and not here
+ */
+import {
+  IComponents as IBaseComponents,
+  useUIComponents as useBaseUIComponents,
+} from "@bluelibs/x-ui-react-bundle";
+
+export interface IComponents extends IBaseComponents {}
+
+export function useUIComponents(): IComponents {
+  return useBaseUIComponents();
+}
