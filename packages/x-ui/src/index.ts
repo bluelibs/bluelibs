d349d1299215c92255bd582592cce5f008e487a0
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-10 17:01:25 +0200

    introduced compatbility

--- packages/x-ui/src/index.ts
+++ packages/x-ui/src/index.ts
@@ -15,17 +15,17 @@ import { IReactRoute } from "@bluelibs/x-ui-react-router-bundle";
  */
 export interface IRoute extends IReactRoute {}
 
-// /**
-//  * The code below is designed to offer backwards compatibility
-//  * We recommend that interface extension should be done on `@bluelibs/x-ui-react-bundle` and not here
-//  */
-// import {
-//   IComponents as IBaseComponents,
-//   useUIComponents as useBaseUIComponents,
-// } from "@bluelibs/x-ui-react-bundle";
+/**
+ * The code below is designed to offer backwards compatibility
+ * We recommend that interface extension should be done on `@bluelibs/x-ui-react-bundle` and not here
+ */
+import {
+  IComponents as IBaseComponents,
+  useUIComponents as useBaseUIComponents,
+} from "@bluelibs/x-ui-react-bundle";
 
-// export interface IComponents extends IBaseComponents {}
+export interface IComponents extends IBaseComponents {}
 
-// export function useUIComponents(): IComponents {
-//   return useBaseUIComponents();
-// }
+export function useUIComponents(): IComponents {
+  return useBaseUIComponents();
+}
