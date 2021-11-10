1c3ce2b9e306615f74dd84f1f13f7d5cf8afa765
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-10 17:00:22 +0200

    (i react route)

--- packages/x-ui/src/index.ts
+++ packages/x-ui/src/index.ts
@@ -8,3 +8,24 @@ export * from "@bluelibs/ui-apollo-bundle";
 
 export * from "./XUIBundle";
 export * from "./defs";
+
+import { IReactRoute } from "@bluelibs/x-ui-react-router-bundle";
+/**
+ * We use `IRoute` here for compatibility reasons
+ */
+export interface IRoute extends IReactRoute {}
+
+// /**
+//  * The code below is designed to offer backwards compatibility
+//  * We recommend that interface extension should be done on `@bluelibs/x-ui-react-bundle` and not here
+//  */
+// import {
+//   IComponents as IBaseComponents,
+//   useUIComponents as useBaseUIComponents,
+// } from "@bluelibs/x-ui-react-bundle";
+
+// export interface IComponents extends IBaseComponents {}
+
+// export function useUIComponents(): IComponents {
+//   return useBaseUIComponents();
+// }
