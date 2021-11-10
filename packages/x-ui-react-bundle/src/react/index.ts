d8a8b09a743117463ba1fa48f8b2d127aaf71deb
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-10 16:50:44 +0200

    test

--- packages/x-ui-react-bundle/src/react/index.ts
+++ packages/x-ui-react-bundle/src/react/index.ts
@@ -3,5 +3,6 @@ export * from "./smart";
 export * from "./XUIProvider";
 
 export * as Components from "./components";
+export { IComponents } from "./components/types";
 
 export { XUIProvider } from "./XUIProvider";
