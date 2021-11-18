ed63acf1eebdaa3d348ab0ca824cfb6d20a3c66b
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-18 13:01:26 +0200

    fix(x-ui-i18n-bundle): deprecation fixes

--- packages/x-ui-i18n-bundle/src/XUII18NBundle.ts
+++ packages/x-ui-i18n-bundle/src/XUII18NBundle.ts
@@ -1,12 +1,12 @@
 import { Bundle } from "@bluelibs/core";
 import { I18N_CONFIG_TOKEN } from ".";
-import { IUII18NBundleConfig } from "./defs";
+import { IXUII18NBundleConfig } from "./defs";
 
-export class XUII18NBundle extends Bundle<IUII18NBundleConfig> {
+export class XUII18NBundle extends Bundle<IXUII18NBundleConfig> {
   protected defaultConfig = {
     defaultLocale: "en",
     polyglots: [],
-  } as IUII18NBundleConfig;
+  } as IXUII18NBundleConfig;
 
   async prepare() {
     this.container.set(I18N_CONFIG_TOKEN, this.config);
