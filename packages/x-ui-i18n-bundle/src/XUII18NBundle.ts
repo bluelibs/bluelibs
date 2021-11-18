cbeb39c9da38a7ba96f02a9ed763e47bb1ae5441
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-18 12:50:35 +0200

    fix(x-ui-i18n-bundle): deprecated UI and replaced with XUI as prefix for the bundle

--- packages/x-ui-i18n-bundle/src/XUII18NBundle.ts
+++ packages/x-ui-i18n-bundle/src/XUII18NBundle.ts
@@ -2,13 +2,12 @@ import { Bundle } from "@bluelibs/core";
 import { I18N_CONFIG_TOKEN } from ".";
 import { IUII18NBundleConfig } from "./defs";
 
-export class UII18NBundle extends Bundle<IUII18NBundleConfig> {
+export class XUII18NBundle extends Bundle<IUII18NBundleConfig> {
   protected defaultConfig = {
     defaultLocale: "en",
     polyglots: [],
   } as IUII18NBundleConfig;
 
-  // TODO: is the token reaally needed?
   async prepare() {
     this.container.set(I18N_CONFIG_TOKEN, this.config);
   }
--- packages/x-ui-i18n-bundle/src/index.ts
+++ packages/x-ui-i18n-bundle/src/index.ts
@@ -1,5 +1,11 @@
 export * from "./react";
 export * from "./defs";
 export * from "./constants";
+import { XUII18NBundle } from "./XUII18NBundle";
 
-export * from "./UII18NBundle";
+export * from "./XUII18NBundle";
+
+/**
+ * @deprecated Please use XUII18NBundle
+ */
+export const UII18NBundle = XUII18NBundle;
