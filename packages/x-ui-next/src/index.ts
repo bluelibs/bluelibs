104076369a0379cc25e62baab2b22d4802f80375
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-19 11:21:22 +0200

    fix(x-ui-next): umbrella package exports

--- packages/x-ui-next/src/index.ts
+++ packages/x-ui-next/src/index.ts
@@ -4,5 +4,7 @@ export * from "./defs";
 export * from "@bluelibs/x-ui-guardian-bundle";
 export * from "@bluelibs/x-ui-react-bundle";
 export * from "@bluelibs/x-ui-router";
+export * from "@bluelibs/x-ui-collections-bundle";
+export * from "@bluelibs/x-ui-i18n-bundle";
 
 export * from "./XUINextBundle";
