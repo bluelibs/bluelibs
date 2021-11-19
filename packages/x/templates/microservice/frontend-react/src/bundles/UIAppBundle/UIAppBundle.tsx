d650983b2088638514d3f9efaa9dc7bb92b86223
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-11-19 16:53:43 +0200

    fix order for loading ui app

--- packages/x/templates/microservice/frontend-react/src/bundles/UIAppBundle/UIAppBundle.tsx
+++ packages/x/templates/microservice/frontend-react/src/bundles/UIAppBundle/UIAppBundle.tsx
@@ -6,16 +6,13 @@ import { i18n } from "./i18n";
 // import * as ComponentOverrides from "./overrides";
 
 export class UIAppBundle extends Bundle {
-  async prepare() {
+  async init() {
+    // In case you want to override certain components
+    // xui.updateComponents(ComponentOverrides);
     const xui = this.container.get(XUIBundle);
     xui.setGuardianClass(AppGuardian);
     xui.storeI18N(i18n);
 
-    // In case you want to override certain components
-    // xui.updateComponents(ComponentOverrides);
-  }
-
-  async init() {
     // All routes are added via the routing service
     const router = this.container.get(XRouter);
     router.add(Routes);
