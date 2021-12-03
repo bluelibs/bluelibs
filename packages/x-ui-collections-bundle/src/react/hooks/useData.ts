66675b3c02779333da58091a162c03a6e0ce23ab
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-12-03 09:11:24 +0200

    feat(x-ui-collections-bundle): added refetch() capability

--- packages/x-ui-collections-bundle/src/react/hooks/useData.ts
+++ packages/x-ui-collections-bundle/src/react/hooks/useData.ts
@@ -1,14 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { Constructor } from "@bluelibs/core";
 import { Collection, IQueryInput } from "../../graphql/Collection";
 import { QueryBodyType } from "../../graphql/defs";
 
 import { use } from "@bluelibs/x-ui-react-bundle";
 
+type RefetchType = () => Promise<void>;
+
 export type UseDataStateType<T> = {
   data: T | null;
   error: Error | null;
   isLoading: boolean;
+  refetch: RefetchType;
 };
 
 /**
@@ -18,33 +21,42 @@ export function useData<T>(
   collectionClass: Constructor<Collection<T>>,
   queryOptions: IQueryInput<T> = {},
   body: QueryBodyType<T>
-  // options: QueryOp = {},
 ): UseDataStateType<Partial<T>[]> {
   const collection = use(collectionClass);
+  const requestSignature = JSON.stringify(queryOptions);
+
+  const fetch = useMemo<RefetchType>(() => {
+    return async () =>
+      collection
+        .find(queryOptions, body)
+        .then((documents) => {
+          setDataState({
+            data: documents,
+            isLoading: false,
+            error: null,
+            refetch: fetch,
+          });
+        })
+        .catch((err) => {
+          setDataState({
+            data: null,
+            isLoading: false,
+            error: err,
+            refetch: fetch,
+          });
+        });
+  }, [requestSignature]);
+
   const [dataState, setDataState] = useState<UseDataStateType<Partial<T>[]>>({
     data: null,
     error: null,
     isLoading: true,
+    refetch: fetch,
   });
 
   useEffect(() => {
-    collection
-      .find(queryOptions, body)
-      .then((documents) => {
-        setDataState({
-          data: documents,
-          isLoading: false,
-          error: null,
-        });
-      })
-      .catch((err) => {
-        setDataState({
-          data: null,
-          isLoading: false,
-          error: err,
-        });
-      });
-  }, [JSON.stringify(queryOptions)]);
+    fetch();
+  }, [requestSignature]);
 
   return dataState;
 }
@@ -60,30 +72,40 @@ export function useDataOne<T>(
 ): UseDataStateType<Partial<T>> {
   const filters = { _id };
   const collection = use(collectionClass);
+  const requestSignature = _id.toString() + JSON.stringify(body);
+
+  const fetch = useMemo<RefetchType>(() => {
+    return async () =>
+      collection
+        .findOne({ filters }, body)
+        .then((document) => {
+          setDataState({
+            data: document,
+            isLoading: false,
+            error: null,
+            refetch: fetch,
+          });
+        })
+        .catch((err) => {
+          setDataState({
+            data: null,
+            isLoading: false,
+            error: err,
+            refetch: fetch,
+          });
+        });
+  }, [requestSignature]);
+
   const [dataState, setDataState] = useState<UseDataStateType<Partial<T>>>({
     data: null,
     error: null,
     isLoading: true,
+    refetch: fetch,
   });
 
   useEffect(() => {
-    collection
-      .findOne({ filters }, body)
-      .then((document) => {
-        setDataState({
-          data: document,
-          error: null,
-          isLoading: false,
-        });
-      })
-      .catch((err) => {
-        setDataState({
-          data: null,
-          error: err.toString(),
-          isLoading: false,
-        });
-      });
-  }, [JSON.stringify(filters)]);
+    fetch();
+  }, [requestSignature]);
 
   return dataState;
 }
