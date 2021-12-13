c3a2e5f8fd9aa48e1e75e6233f4a65bafc5e85f4
qfl1ck32 <andrei.rusu@cultofcoders.com>
2021-12-13 16:19:08 +0200

    (update router)

--- packages/x-ui-next/src/react/router/NextRouter.ts
+++ packages/x-ui-next/src/react/router/NextRouter.ts
@@ -5,33 +5,20 @@ import {
   XCoreRouter,
 } from "@bluelibs/x-ui-router";
 
-import { NextRouter } from "next/router";
 import BaseNextRouter from "next/router";
 
-import { Constructor, Service } from "@bluelibs/core";
+import { Service } from "@bluelibs/core";
 
-export type XNextRouterClass = XCoreRouter<IRoute> & NextRouter;
-
-export const XNextRouter = ((): Constructor<XNextRouterClass> => {
-  @Service()
-  class XNextRouterClass extends XCoreRouter<IRoute> {
-    constructor() {
-      super();
-
-      Object.assign(this, BaseNextRouter.router);
-
-      BaseNextRouter.router.events.on("routeChangeComplete", () => {
-        Object.assign(this, BaseNextRouter.router);
-      });
-    }
-
-    go<T extends IRouteParams, Q extends IRouteParams>(
-      route: IRoute<T, Q>,
-      options?: IRouteGenerationProps<T, Q>
-    ): void {
-      BaseNextRouter.push(this.path(route, options));
-    }
+@Service()
+export class XNextRouter extends XCoreRouter<IRoute> {
+  get next() {
+    return BaseNextRouter;
   }
 
-  return XNextRouterClass as any;
-})();
+  go<T extends IRouteParams, Q extends IRouteParams>(
+    route: IRoute<T, Q>,
+    options?: IRouteGenerationProps<T, Q>
+  ): void {
+    this.next.push(this.path(route, options));
+  }
+}
