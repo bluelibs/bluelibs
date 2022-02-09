5461155bb71eb724c4e31caa2cbde4e4ec3bc21e
Youness MOUFAKKIR <youness.moufakkir@gmail.com>
2022-02-09 15:29:47 +0100

    latest changes on cache management+unit testing

--- packages/x-bundle/src/cache/CacheService.ts
+++ packages/x-bundle/src/cache/CacheService.ts
@@ -2,12 +2,16 @@ import { Inject, Service } from "@bluelibs/core";
 import { CACHE_CONFIG } from "../constants";
 import { CacheOptions } from "./defs";
 import * as CacheManager from "cache-manager";
+import { LoggerService } from "@bluelibs/logger-bundle";
 
 @Service()
 export class CacheService {
   private cacheManager;
 
-  constructor(@Inject(CACHE_CONFIG) private config) {
+  constructor(
+    @Inject(CACHE_CONFIG) private config,
+    protected readonly logger: LoggerService
+  ) {
     this.cacheManager = CacheManager.caching({
       store: this.config.store,
       ...this.config.storeConfig,
@@ -30,22 +34,21 @@ export class CacheService {
   async get(key: string) {
     const cachedData = await this.cacheManager.get(key);
     if (!cachedData) return { found: false, data: undefined };
-    else {
-      if (cachedData && cachedData.refresh) {
-        await this.set(key, cachedData.data, {
-          ttl: cachedData.ttl,
-          refresh: true,
-        });
-      }
-      console.log("cachedData:---------------------------------");
-      console.log({
-        syncedAt: cachedData.syncedAt,
+
+    if (cachedData && cachedData.refresh) {
+      await this.set(key, cachedData.data, {
         ttl: cachedData.ttl,
-        refresh: cachedData.refresh,
-        key: cachedData,
+        refresh: true,
       });
-      return { found: true, data: cachedData.data };
     }
+    this.logger.info("Cached Data:", {
+      syncedAt: cachedData.syncedAt,
+      ttl: cachedData.ttl,
+      refresh: cachedData.refresh,
+      key: key,
+    });
+
+    return { found: true, data: cachedData.data };
   }
   async keys() {
     return await this.cacheManager.keys();
