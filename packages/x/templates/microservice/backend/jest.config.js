e96b8be85953685370ac01e8797d77ecbf1671a7
qfl1ck32 <rusuandreicristian32@gmail.com>
2022-02-10 12:45:30 +0200

    (add): module name mapper

--- packages/x/templates/microservice/backend/jest.config.js
+++ packages/x/templates/microservice/backend/jest.config.js
@@ -1,4 +1,18 @@
+const tsconfig = require("./tsconfig.json");
+
+const getModuleNameMapper = () => {
+  const { paths } = tsconfig.compilerOptions
+
+  return Object.keys(paths).reduce((mappedModuleNames, moduleName) => ({
+    ...mappedModuleNames,
+
+    [`${moduleName.slice(0, -1)}(.*)`]: `<rootDir>/${paths[moduleName][0].slice(0, -1)}/$1`
+  }), {})
+}
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+
+  moduleNameMapper: getModuleNameMapper()
 };
