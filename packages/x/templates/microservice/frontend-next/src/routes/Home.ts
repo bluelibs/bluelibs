564000ba3970c93a089d87c88010e6799919ecc4
Theodor Diaconu <diaconu.theodor@gmail.com>
2022-02-23 16:37:48 +0200

    fix(x): nextjs boilerplate to contain graphql-codegen, routing simplicity and removal of x-ui-admin as deps

--- packages/x/templates/microservice/frontend-next/src/routes/Home.ts
+++
@@ -1,6 +0,0 @@
-import { IRoute } from "@bluelibs/x-ui-router";
-
-export const Home: IRoute = {
-  name: "Home",
-  path: "/",
-};
--- packages/x/templates/microservice/frontend-next/src/routes/index.ts
+++ packages/x/templates/microservice/frontend-next/src/routes/index.ts
@@ -1,1 +1,6 @@
-export * from "./Home";
+import { IRoute } from "@bluelibs/x-ui-router";
+
+export const Home: IRoute = {
+  name: "Home",
+  path: "/",
+};
