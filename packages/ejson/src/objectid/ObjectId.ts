390937397dcab16ee5e291827ae1f79090e3380b
Theodor Diaconu <diaconu.theodor@gmail.com>
2022-03-24 09:20:13 +0200

    fix(ejson): objectIds

--- packages/ejson/src/objectid/ObjectId.ts
+++ packages/ejson/src/objectid/ObjectId.ts
@@ -27,7 +27,7 @@ const kId = Symbol("id");
  * @public
  */
 export class ObjectId {
-  _bsontype!: "ObjectId";
+  _bsontype?: any;
 
   /** @internal */
   static index = Math.floor(Math.random() * 0xffffff);
