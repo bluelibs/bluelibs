323cbd6c7460e23fcb71bb78d2c49245b667c76c
Theodor Diaconu <diaconu.theodor@gmail.com>
2022-05-16 17:11:23 +0300

    feat(x-bundle): added option to perform partial ToModel and ToValidate as executors

--- packages/x-bundle/src/executors/model-and-validation.ts
+++ packages/x-bundle/src/executors/model-and-validation.ts
@@ -1,22 +1,39 @@
-import { toModel } from "@bluelibs/ejson";
+import { toModel, ToModelOptions } from "@bluelibs/ejson";
 import { ValidatorService, IValidateOptions } from "@bluelibs/validator-bundle";
 
 export interface IToModelExecutorOptions {
   field?: string;
+  /**
+   * Use this for Update methods in which you don't want to set the default values.
+   */
+  partial?: boolean;
 }
 
 export function ToModel(model: any, options: IToModelExecutorOptions = {}) {
   if (!options.field) {
     options.field = "input";
   }
+  let toModelOptions: Partial<ToModelOptions> = {};
+  if (options.partial) {
+    toModelOptions.partial = true;
+  }
 
   return async function ToModel(_, args, ctx, ast) {
-    args[options.field] = toModel(model, args[options.field]);
+    args[options.field] = toModel(
+      model,
+      args[options.field],
+      toModelOptions as ToModelOptions
+    );
   };
 }
 
 export interface IValidateExecutorOptions extends IValidateOptions {
   field?: string;
+  /**
+   * Use this option as true when you are performing validation only on the specified fields. Commonly used when you have default values on your model.
+   * Alias for `stripUnknown` behind the scenes, named this way for convenience when using ToModel with the same method.
+   */
+  partial?: boolean;
 }
 
 export function Validate(options: IValidateExecutorOptions = {}) {
@@ -29,6 +46,7 @@ export function Validate(options: IValidateExecutorOptions = {}) {
 
     await validator.validate(args[options.field], {
       ...options,
+      stripUnknown: options.partial === true ? true : false,
     });
   };
 }
