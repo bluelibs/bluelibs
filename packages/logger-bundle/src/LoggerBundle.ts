606e848bf9e1a9078b722d8c15e08e2bd31e27de
Theodor Diaconu <diaconu.theodor@gmail.com>
2025-02-17 13:20:15 +0200

    Allow configuring LoggerBundle to only log stuff above an importance threshold
    Fixes #362
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/bluelibs/bluelibs/issues/362?shareId=XXXX-XXXX-XXXX-XXXX).

--- packages/logger-bundle/src/LoggerBundle.ts
+++ packages/logger-bundle/src/LoggerBundle.ts
@@ -1,11 +1,12 @@
 import { Bundle } from "@bluelibs/core";
-import { ILoggerBundleConfig } from "./defs";
+import { ILoggerBundleConfig, LogLevel } from "./defs";
 import { ConsoleListener } from "./listeners/ConsoleListener";
 import { LoggerService } from "./services/LoggerService";
 
 export class LoggerBundle extends Bundle<ILoggerBundleConfig> {
   defaultConfig = {
     console: true,
+    level: LogLevel.INFO,
   };
 
   async prepare() {
